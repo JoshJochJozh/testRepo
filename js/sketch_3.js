8cb6fef2eb38bcad3cbeadd31a8c041e7dd147b3
JoshJochJozh <josh@dyn-160-39-236-128.dyn.columbia.edu>
2017-03-05 12:39:33 -0500

    assaignment 3

--- js/sketch_3.js
+++ js/sketch_3.js
@@ -1,64 +1,23 @@
 // ***** Global variables ***** //
 var refugeeTable;
-var maxTotal = 0;
-var maxLabel = 0;
-var maxLength = 550;
-
-// ***** Preload function ***** //
-function preload(){
-  refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header');
-  console.log('Done loading table...');
-}
-
-// ***** Setup function ***** //
-function setup(){
-  createCanvas(800, 3000);
-  textSize(12);
-  console.log('Setup complete...');
-  print(refugeeTable.getRowCount() + ' rows loaded...');
-  print(refugeeTable.getColumnCount() + ' columns loaded...');
-  for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-    maxTotal = max(refugeeTable.getNum(i, 'Total'), maxTotal);
-    maxLabel = max(refugeeTable.getString(i, 'Country').length, maxLabel);
-  }
-  print('Maximum total is ' + maxTotal);
-  print('Maximum label length is ' + maxLabel);
-}
-
-// ***** Draw function ***** //
-function draw(){
-    background(255);
-    fill(0);
-    noStroke();
-    textAlign(LEFT, TOP);
-    for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-        var total = refugeeTable.getNum(i, 'Total');
-        var length = map(total, 0, maxTotal, 0, maxLength);
-        rect(maxLabel * 5, 2 + 14*i, length, 12);
-        text(nfc(total, 0), maxLabel * 5 + length + 5, 14*i);
-    }
-    textAlign(RIGHT, TOP);
-    for (var i = 0; i < refugeeTable.getRowCount(); i++) {
-        text(refugeeTable.getString(i, 'Country'), maxLabel * 5 - 5, 14*i);
-    }
-}
-Now, let's say that you only want to select the countries with more than 100,000 refugees. The way to do this would be to create a new table and add just the records that match the selection criteria.
-// ***** Global variables ***** //
-var refugeeTable;
 var topRefugeesTable = new p5.Table;
 var maxTotal = 0;
 var maxLabel = 0;
 var maxLength = 550;
 
+var headers = ['Country','Refugees','Asylum-seekers','Returned refugees','IDPs','Returned IDPs','Stateless','Others of concern','Total']
+
+
+
 // ***** Preload function ***** //
 function preload(){
-    refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header');
+    refugeeTable = loadTable('../data/RefugeesUNHCR_2.csv', 'csv', 'header');
     console.log('Done loading table...');
 }
 
 // ***** Setup function ***** //
 function setup(){
-    createCanvas(800, 3000);
+    createCanvas(1600, 800);
     textSize(12);
     console.log('Setup complete...');
     print(refugeeTable.getRowCount() + ' rows loaded...');
@@ -74,32 +33,39 @@ function setup(){
 
 // ****** Create new table function ******** //
 function createNewTable(){
-    topRefugeesTable.addColumn('Country');
-    topRefugeesTable.addColumn('Total');
+  for (var i=0; i< headers.length; i++) {
+    topRefugeesTable.addColumn(headers[i]);
+
+  }
+
     for (var i = 0; i < refugeeTable.getRowCount(); i++) {
         var totalRefugees = refugeeTable.getNum(i, 'Total');
         if (totalRefugees >= 100000) {
             var newRow = topRefugeesTable.addRow()
-            newRow.setString('Country', refugeeTable.getString(i, 'Country'));
-            newRow.setNum('Total', refugeeTable.getNum(i, 'Total'));
+            for (var j =0; j < headers.length;j++) {
+              newRow.setString(headers[j], refugeeTable.getString(i, headers[j]));
+            }
         }
     }
+  
     print('New top refugee table created...');
+    print(topRefugeesTable);
 }
 
-function drawCountries(tableName){
-    fill(0);
+function drawCountries(category){
+    fill(183, 28, 28);
     noStroke();
-    textAlign(LEFT, TOP);
-    for (var i = 0; i < tableName.getRowCount(); i++) {
-        var total = tableName.getNum(i, 'Total');
+    textAlign(LEFT, BOTTOM);
+    for (var i = 0; i < topRefugeesTable.getRowCount(); i++) {
+        var total = topRefugeesTable.getNum(i, category);
         var length = map(total, 0, maxTotal, 0, maxLength);
-        rect(maxLabel * 5, 2 + 14*i, length, 12);
-        text(nfc(total, 0), maxLabel * 5 + length + 5, 14*i);
+        rect( 160 + 25*i , maxLabel * 5, 12, length);
+        text(nfc(total, 0), 160+25*i, maxLabel * 5 + length + 15);
+        text(topRefugeesTable.getString(i, 'Country'),160+25*i, maxLabel * 5 + length + 25);
     }
-    textAlign(RIGHT, TOP);
-    for (var i = 0; i < tableName.getRowCount(); i++) {
-        text(tableName.getString(i, 'Country'), maxLabel * 5 - 5, 14*i);
+    textAlign(RIGHT, BOTTOM);
+    for (var i = 0; i < topRefugeesTable.getRowCount(); i++) {
+        //text(topRefugeesTable.getString(i, 'Country'),25*i, maxLabel * 5 + length + 25);
     }
 }
 
@@ -107,5 +73,5 @@ function drawCountries(tableName){
 function draw(){
     background(255);
     // drawCountries(refugeeTable);
-    drawCountries(topRefugeesTable);
+    drawCountries('Total');
 }
