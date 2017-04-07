70cbaf35bb437148a531d83ad92fc4b88231275a
JoshJochJozh <josh@dyn-160-39-236-216.dyn.columbia.edu>
2017-04-07 14:20:55 -0400

    test_6

--- js/test_6.js
+++ js/test_6.js
@@ -1,24 +1,147 @@
-// ***** Global variables ***** //
-var weatherData;
 
-// ***** Setup function ***** //
-function setup(){
-    createCanvas(800, 800);
-    loadJSON('../data/Refugee_2.json', getWeatherData);
+
+
+
+var nervousBall = [];
+var blackWhole;
+
+
+
+
+
+
+
+
+
+function setup () {
+  createCanvas(800,800);
+
+  for (var i = 0; i < 50; i++) {
+    nervousBall.push(new Jitter());
+  }
+
+  blackWhole = createVector(400, 400);
 }
 
-function getWeatherData(apiData){
-  weatherData = apiData;
-  console.log(weatherData);
-}
 
-// ***** Draw function ***** //
-function draw(){
-    background(255);
-    if (weatherData) {
-      text(weatherData, 20, 20);
+
+
+function Jitter() {
+
+  //**** Properties
+
+  this.position = createVector(random(0,width), random(0,height));
+
+
+  this.diameter = random(0,60);
+
+  this.r = random(20,80);
+  this.g = random(50,100);
+  this.b = random(50,100);
+
+  this.speed = random(0,5);
+
+
+  this.velocityX = random(-4,4);
+  this.velocityY = random(-4,4);
+
+
+  this.panic =false;
+
+
+
+
+  //***** Functions
+
+  this.display = function() {
+
+    if (this.panic == true) {
+      fill(255,0,0);
+    } else {
+      fill(this.r,this.g,this.b);
     }
-    else{
-      text('Loading...', 20, 20);
+
+
+    colorMode(HSB,100);
+
+
+
+
+    noStroke();
+    ellipse(this.position.x,this.position.y, this.diameter, this.diameter);
+  }
+
+  this.update = function() {
+    this.position.x = this.position.x + random(-this.speed,this.speed);
+    this.position.y = this.position.y + random(-this.speed,this.speed);
+
+  }
+
+  this.move = function() {
+    this.position.x = this.position.x + this.velocityX;
+    this.position.y = this.position.y + this.velocityY;
+
+  }
+
+
+  this.calculateDistance = function() {
+
+    var distance = this.position.dist(blackWhole);
+
+    if(distance > 400) {
+      this.velocityX = -this.velocityX;
+      this.velocityY = -this.velocityY;
     }
+
+    if(distance < 150) {
+      this.panic = true;
+    }
+
+    if(distance > 150) {
+      this.panic = false;
+    }
+
+  }
+
+
+
+
+
 }
+
+
+
+
+function draw() {
+  background(255);
+
+
+  fill(0,100,100,0.01);
+  rect(0,0,width,height);
+
+  fill(0);
+  ellipse(blackWhole.x, blackWhole.y, 800, 800);
+
+  stroke(255);
+  strokeWeight(1);
+  fill(255);
+  ellipse(blackWhole.x, blackWhole.y, 300, 300);
+
+
+  for (var i = 0; i < 50; i++) {
+    
+
+    nervousBall[i].display();
+    nervousBall[i].update();
+
+    nervousBall[i].move();
+
+    nervousBall[i].calculateDistance();
+
+
+
+  }
+
+
+}
+
